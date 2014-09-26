22a3bf8e6b36938fd7f87efcb5378e390cbd6789
angadanand <angad.anand2@gmail.com>
2014-09-26 04:37:43 -0700

    Hacked up some basic UI

---
+++ src/main/webapp/scripts/js/index.js
@@ -0,0 +1,10 @@
+/*
+ * Hacking away at some JQuery and Java Script
+ */
+
+$(document).ready(function(){
+	
+	$("#btn_addtodo").click(function(){
+		$("#success").css("visibility","visible");
+	});
+});
