8d94eafdef5f5396de8a7b0a6ae37f3a4c067d08
Angad Anand <angad.anand2@gmail.com>
2015-11-07 17:25:22 -0800

    Update TwilioConfiguration.java

--- src/main/java/com/todo/configuration/TwilioConfiguration.java
+++ src/main/java/com/todo/configuration/TwilioConfiguration.java
@@ -3,8 +3,8 @@ package com.todo.configuration;
 import com.twilio.sdk.TwilioRestClient;
 
 public class TwilioConfiguration {
-	private final String ACCOUNT_SID = "ACb4c333fecc7f9bff91464cc2c4ff4724";
-	private final String AUTH_TOKEN = "e33dd91fd7f00cc218217fae1377ac9d";
+	private final String ACCOUNT_SID = " Acound SID ";
+	private final String AUTH_TOKEN = "Twilio auth token";
 	
 	public TwilioConfiguration(){}
 	
